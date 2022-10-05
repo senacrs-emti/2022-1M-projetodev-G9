b4927898148828c8b1dc3cabeed07c346545ca21
Cruz <henriquecc2006@gmail.com>
2022-10-05 19:28:14 -0300

    sla

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-let sombra = document.getElementById('hd');
-let i = 0;
-	while(i<1000){
-        i++;
-        console.log(i)			
-    if(i == 300){
-        sombra.setAttribute('style', 'box-shadow: red 5px 5px 5px 5px');
-        // alert('laranja');
-    }else if(i == 600){
-        sombra.setAttribute('style', 'box-shadow: orange 5px 5px 5px 5px');
-        // alert('amarelo');
-    }else if(i == 900){
-        sombra.setAttribute('style', 'box-shadow: yellow 5px 5px 5px 5px');
-        //teste = 0;
-    }
-}
+
     
