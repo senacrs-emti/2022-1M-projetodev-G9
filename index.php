9ca62b24dff7258945d1560c122e19449604a740
CRUZ1807 <henriquecc2006@gmail.com>
2022-10-10 11:44:36 -0300

    jsjss

---
+++ index.php
@@ -0,0 +1,240 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!--nosso css-->
+    <link rel="stylesheet" href="style.css">
+    <title>Simulador de química</title>
+  </head>
+  <body>
+    <header id="hd">
+        <h1>TABELA PERIÓDICA</h1>
+    </header>
+    <table>
+      <tr class="l1">
+        <td class="nm c1">
+            <p>1</p>
+            <h1>H</h1>
+        </td>
+        <td class="c2"></td>
+        <td class="c3"></td>
+        <td class="c4"></td>
+        <td class="c5"></td>
+        <td class="c6"></td>
+        <td class="c7"></td>
+        <td class="c8"></td>
+        <td class="c9"></td>
+        <td class="c10"></td>
+        <td class="c11"></td>
+        <td class="c12"></td>
+        <td class="c13"></td>
+        <td class="c14"></td>
+        <td class="c15"></td>
+        <td class="c16"></td>
+        <td class="c17"></td>
+        <td class="c18 gn" id="he">
+            <p>2</p>
+            <h1 id="he">He</h1>
+        </td>
+      </tr>
+      <tr class="l2">
+        <td class="c1 ma">
+          <p>3</p>
+          <h1>Li</h1>
+        </td>
+        <td class="c2 mat" id="ajuste">
+          <p>4</p>
+          <h1>Be</h1>
+        </td>
+        <td class="c3"></td>
+        <td class="c4"></td>
+        <td class="c5"></td>
+        <td class="c6"></td>
+        <td class="c7"></td>
+        <td class="c8"></td>
+        <td class="c9"></td>
+        <td class="c10"></td>
+        <td class="c11"></td>
+        <td class="c12"></td>
+        <td class="c13 sm">
+          <p>5</p>
+          <h1>B</h1>
+        </td>
+        <td class="c14 nm">
+          <p>6</p>
+          <h1>C</h1>
+        </td>
+        <td class="c15 nm">
+          <p>7</p>
+          <h1>N</h1>
+        </td>
+        <td class="c16 nm">
+          <p>8</p>
+          <h1>O</h1>
+        </td>
+        <td class="c17 h">
+          <p>9</p>
+          <h1>F</h1>
+        </td>
+        <td class="c18 gn" id="ajuste">
+          <p>10</p>
+          <h1>Ne</h1>
+        </td>
+      </tr>
+      <tr class="l3">
+        <td class="c1 ma" id="ajuste">
+          <p>11</p>
+          <h1>Na</h1>
+        </td>
+        <td class="c2 mat" id="ajuste">
+          <p>12</p>
+          <h1>Mg</h1>
+        </td>
+        <td class="c3"></td>
+        <td class="c4"></td>
+        <td class="c5"></td>
+        <td class="c6"></td>
+        <td class="c7"></td>
+        <td class="c8"></td>
+        <td class="c9"></td>
+        <td class="c10"></td>
+        <td class="c11"></td>
+        <td class="c12"></td>
+        <td class="c13 om" id="ajuste">
+          <p>13</p>
+          <h1>Al</h1>
+        </td>
+        <td class="c14 sm" id="ajuste">
+          <p>14</p>
+          <h1>Si</h1>
+        </td>
+        <td class="c15 nm">
+          <p>15</p>
+          <h1>P</h1>
+        </td>
+        <td class="c16 nm">
+          <p>16</p>
+          <h1>S</h1>
+        </td>
+        <td class="c17 h" id="ajuste">
+          <p>17</p>
+          <h1>Cl</h1>
+        </td>
+        <td class="c18 gn" id="ajuste">
+          <p>18</p>
+          <h1>Ar</h1>
+        </td>
+      </tr>
+      <tr class="l4">
+        <td class="c1 ma">
+          <p>19</p>
+          <h1>K</h1>
+        </td>
+        <td class="c2 mat" id="ajuste">
+          <p>20</p>
+          <h1>Ca</h1>
+        </td>
+        <td class="c3 mt" id="ajuste">
+          <p>21</p>
+          <h1>Sc</h1>
+        </td>
+        <td class="c4 mt" id="ajuste">
+          <p>22</p>
+          <h1>Ti</h1>
+        </td>
+        <td class="c5 mt">
+          <p>23</p>
+          <h1>V</h1>
+        </td>
+        <td class="c6 mt" id="ajuste">
+          <p>24</p>
+          <h1>Cr</h1>
+        </td>
+        <td class="c7 mt" id="ajuste">
+          <p>25</p>
+          <h1>Mn</h1>
+        </td>
+        <td class="c8 mt" id="ajuste">
+          <p>26</p>
+          <h1>Fe</h1>
+        </td>
+        <td class="c9 mt" id="ajuste">
+          <p>27</p>
+          <h1>Co</h1>
+        </td>
+        <td class="c10 mt" id="ajuste">
+          <p>28</p>
+          <h1>Ni</h1>
+        </td>
+        <td class="c11 mt" id="ajuste">
+          <p>29</p>
+          <h1>Cu</h1>
+        </td>
+        <td class="c12 mt" id="ajuste">
+          <p>30</p>
+          <h1>Zn</h1>
+        </td>
+        <td class="c13 om" id="ajuste">
+          <p>31</p>
+          <h1>Ga</h1>
+        </td>
+        <td class="c14 sm" id="ajuste">
+          <p>32</p>
+          <h1>Ge</h1>
+        </td>
+        <td class="c15 sm" id="ajuste">
+          <p>33</p>
+          <h1>As</h1>
+        </td>
+        <td class="c16 nm" id="ajuste">
+          <p>34</p>
+          <h1>Se</h1>
+        </td>
+        <td class="c17 h" id="ajuste">
+          <p>35</p>
+          <h1>Br</h1>
+        </td>
+        <td class="c18 gn" id="ajuste">
+          <p>36</p>
+          <h1>Kr</h1>
+        </td>
+      </tr>
+      <tr class="l5">
+        <td class="c1 ma" id="ajuste">
+          <p>37</p>
+          <h1>Rb</h1>
+        </td>
+        <td class="c2 mat" id="ajuste">
+          <p>38</p>
+          <h1>Sr</h1>
+        </td>
+        <td class="c3 mt">
+          <p>39</p>
+          <h1>Y</h1>
+        </td>
+        <td class="c4 mt" id="ajuste">
+          <p>40</p>
+          <h1>Zr</h1>
+        </td>
+        <td class="c5 mt" id="ajuste">
+          <p>41</p>
+          <h1></h1>
+        </td>
+        <td class="c6"></td>
+        <td class="c7"></td>
+        <td class="c8"></td>
+        <td class="c9"></td>
+        <td class="c10"></td>
+        <td class="c11"></td>
+        <td class="c12"></td>
+        <td class="c13"></td>
+        <td class="c14"></td>
+        <td class="c15"></td>
+        <td class="c16"></td>
+        <td class="c17"></td>
+        <td class="c18"></td>
+      </tr>
+    </table>
+
+   <script src="script.js"></script>
+  </body>
+</html>
